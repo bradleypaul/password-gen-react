563bee50bc82180becb14449c2e8a13667ae7c10
Paul Bradley <bradleypauld@gmail.com>
2020-07-12 19:32:40 -0500

    put generated password in textarea

--- src/App.js
+++ src/App.js
@@ -2,39 +2,61 @@ import React from 'react';
 import generatePassword from './generate';
 import './App.css';
 
-function App() {
-  function generate(e) {
-    console.log(generatePassword({
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = { password: '' };
+  }
+
+  generate() {
+    const password = generatePassword({
       length: 10,
       includeLowercase: true,
       includeUppercase: true,
       includeNumbers: true,
       includeSpecialCharacters: true
-  }));
+    });
+    this.setState((state, props) => {
+      return {
+        password
+      }
+    });
   }
-  return (
-    <div className="App">
-      <header>
-        <h1>Password Generator</h1>
-      </header>
-      <div className="card">
-        <div className="card-header">
-          <h2>Generate a Password</h2>
-        </div>
-        <div className="card-body">
-          <textarea
-            readOnly
-            id="password"
-            placeholder="Your Secure Password"
-            aria-label="Generated Password"
-          ></textarea>
-        </div>
-        <div className="card-footer">
-          <button id="generate" className="btn" onClick={generate}>Generate Password</button>
+
+  check(e) {
+    console.log(e)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Password Generator React</h1>
+        </header>
+        <div className="card">
+          <div className="card-header">
+            <h2>Generate a Password</h2>
+          </div>
+          <div className="card-body">
+            <textarea
+              readOnly
+              id="password"
+              placeholder="Your Secure Password"
+              aria-label="Generated Password"
+              value={this.state.password}
+            ></textarea>
+          </div>
+          <div className="card-footer">
+            <button id="generate" className="btn" onClick={() => this.generate()}>Generate Password</button>
+          </div>
+          {/* Criteria: 
+         <label><input type="checkbox"></input>Include Lowercase</label>
+         <label><input type="checkbox"></input>Include Uppercase</label>
+         <label><input type="checkbox"></input>Include Numbers</label>
+         <label><input type="checkbox" onClick={(e) => this.check(e)}></input>Include Special Characters</label> */}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
