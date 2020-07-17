5c4bffc0cd1497f164e3d60d52e15f386b69f721
Paul Bradley <bradleypauld@gmail.com>
2020-07-16 21:24:03 -0500

    add express

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port)
+app.listen(port, () => {
+  console.log('Server is up!');
+});
